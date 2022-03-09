f5b18ea193d001574d9f1e1b3ae1f2bd5d46068e
unknown <adrian.kanox.2003@gmail.com>
2022-03-09 13:38:54 -0500

    simple api y consumidor

---
+++ consumidor/app.js
@@ -0,0 +1,21 @@
+
+const url = 'http://localhost:3000/usuario';
+const data = {
+    nombre: 'Jose Eduardo',
+    apellido: 'Eustacio Rivera'
+}
+
+async function consultarAPI(){
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+        .then(respuesta => respuesta.json())
+        .then(resultado => console.log(resultado))
+        .catch(error => console.log(error));
+}
+
+consultarAPI();
